d08a778431d32821fdb188c9caf1484de8577305
Quentin Heng <hengquentin@gmail.com>
2018-11-20 22:20:32 +1300

    Implemented Swagger UI for API

--- Startup.cs
+++ Startup.cs
@@ -12,6 +12,7 @@ using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 using Microsoft.EntityFrameworkCore;
 using API_MatchHistory.Models;
+using Swashbuckle.AspNetCore.Swagger;
 
 namespace API_MatchHistory
 {
@@ -31,6 +32,12 @@ namespace API_MatchHistory
 
             services.AddDbContext<MatchHistoryContext>(options =>
                     options.UseSqlite(Configuration.GetConnectionString("MatchHistoryContext")));
+
+            // Register the Swagger generator, defining 1 or more Swagger documents
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new Info { Title = "MatchHistory API", Version = "v1" });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -47,6 +54,17 @@ namespace API_MatchHistory
 
             app.UseHttpsRedirection();
             app.UseMvc();
+
+            // Enable middleware to serve generated Swagger as a JSON endpoint.
+            app.UseSwagger();
+
+            // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.), 
+            // specifying the Swagger JSON endpoint.
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "MatchHistory API V1");
+                c.RoutePrefix = string.Empty; // launch swagger from root
+            });
         }
     }
 }
